13a81f6b1cb1c25385f3c440e7e68404fe3cc85c
lohit <lohit>
2025-06-25 23:14:03 +0530

    fix

--- terraform-files/modules/ami/outputs.tf
+++ terraform-files/modules/ami/outputs.tf
@@ -1,16 +1,16 @@
 output "ami_id" {
-  description = "AMI ID (existing or newly created)"
-  value       = data.aws_ami.existing_ami.id != null ? data.aws_ami.existing_ami.id : aws_ami_from_instance.custom_ami[0].id
+  description = "AMI ID (base Ubuntu ARM64)"
+  value       = data.aws_ami.ubuntu_base.id
 }
 
 output "ami_name" {
   description = "AMI name"
-  value       = data.aws_ami.existing_ami.id != null ? data.aws_ami.existing_ami.name : aws_ami_from_instance.custom_ami[0].name
+  value       = data.aws_ami.ubuntu_base.name
 }
 
 output "ami_creation_date" {
   description = "AMI creation date"
-  value       = data.aws_ami.existing_ami.id != null ? data.aws_ami.existing_ami.creation_date : timestamp()
+  value       = data.aws_ami.ubuntu_base.creation_date
 }
 
 output "ami_architecture" {
@@ -19,6 +19,6 @@ output "ami_architecture" {
 }
 
 output "ami_status" {
-  description = "Whether AMI was created new or existed"
-  value       = data.aws_ami.existing_ami.id != null ? "existing" : "newly_created"
+  description = "AMI source"
+  value       = "base_ubuntu"
 }
